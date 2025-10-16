64bf7547969e0a6a867ae023dcffc7d08fa0222b
Lari Taskula <lari.taskula@hypernova.fi>
2025-10-16 13:54:46 +0300

    Release 24.11.01.1

--- Koha/Plugin/Fi/Hypernova/MelindaComponentPartsImporter.pm
+++ Koha/Plugin/Fi/Hypernova/MelindaComponentPartsImporter.pm
@@ -44,7 +44,7 @@ use Koha::Z3950Servers;
 use Koha::Plugin::Fi::Hypernova::MelindaComponentPartsImporter::Exceptions::Commit;
 use Koha::Plugin::Fi::Hypernova::MelindaComponentPartsImporter::Exceptions::SRUServer;
 
-our $VERSION = "24.11.01.0";
+our $VERSION = "24.11.01.1";
 
 our $metadata = {
     name            => 'Melinda Component Parts Importer',
